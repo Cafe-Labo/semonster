bc146e29a6604d64c446d27a88792ef488d8ed8a
M21A40 <jugglingtosscafe@gmail.com>
2021-07-04 23:25:13 +0900

    v1.1

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMONSETR!!");
+    System.out.println("SEMONSETR!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
   }
