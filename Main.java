87f243d5f9a2449c2a720466ed226ab6eedf7e83
M21A40 <jugglingtosscafe@gmail.com>
2021-07-04 21:54:29 +0900

    add player

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster-v2!!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    //Monster monster = new Monster();
+    //System.out.println(monster.toString());
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
 
